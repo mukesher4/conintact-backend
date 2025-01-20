07b3cc363836f72b65ea85522d06ba47ecaab7a6
Mukesh R <115150217+mukesher4@users.noreply.github.com>
2025-01-20 20:58:38 +0530

    Update groupInviteController.js

--- controllers/groupInviteController.js
+++ controllers/groupInviteController.js
@@ -1,41 +1,41 @@
 const asyncHandler = require("express-async-handler");
 const GroupCode = require("../models/groupCodeModel");
 const GroupUser = require("../models/groupUserModel");
-// const { v4 : uuidv4 } = require('uuid');
+const { v4 : uuidv4 } = require('uuid');
 
 // @desc Map Code to Group
 // @route POST /api/invite/:groupId/
 // @access private
 
-// const generate = asyncHandler(async (req, res) => {
-//     const { groupId } = req.params;
-//     let inviteCode, duplicate;
+const generate = asyncHandler(async (req, res) => {
+    const { groupId } = req.params;
+    let inviteCode, duplicate;
 
-//     // Generate a unique invite code
-//     do {
-//         inviteCode = uuidv4().slice(0, 8);
-//         duplicate = await GroupCode.findOne({ invite_code: inviteCode });
-//     } while (duplicate);
+    // Generate a unique invite code
+    do {
+        inviteCode = uuidv4().slice(0, 8);
+        duplicate = await GroupCode.findOne({ invite_code: inviteCode });
+    } while (duplicate);
 
-//     console.log(groupId);
-//     console.log(inviteCode);
+    console.log(groupId);
+    console.log(inviteCode);
 
-//     // Use updateOne with upsert to handle existing group_id
-//     const response = await GroupCode.updateOne(
-//         { group_id: groupId }, // Filter by group_id
-//         { $set: { invite_code: inviteCode } }, // Set the new invite_code
-//         { upsert: true } // Create a new document if group_id doesn't exist
-//     ).catch((error) => {
-//         console.error("Error creating/updating group invite code:", error);
-//         throw new Error("Failed to create/update group invite code");
-//     });
+    // Use updateOne with upsert to handle existing group_id
+    const response = await GroupCode.updateOne(
+        { group_id: groupId }, // Filter by group_id
+        { $set: { invite_code: inviteCode } }, // Set the new invite_code
+        { upsert: true } // Create a new document if group_id doesn't exist
+    ).catch((error) => {
+        console.error("Error creating/updating group invite code:", error);
+        throw new Error("Failed to create/update group invite code");
+    });
 
-//     if (!response) {
-//         throw new Error('Failed to create/update group invite code');
-//     }
+    if (!response) {
+        throw new Error('Failed to create/update group invite code');
+    }
 
-//     res.status(201).json(inviteCode);
-// });
+    res.status(201).json(inviteCode);
+});
 
 
 // @desc Map User to Group
@@ -86,7 +86,7 @@ const view = asyncHandler(async(req, res) => {
 });
 
 module.exports = {
-	// generate,
+	generate,
 	accept,
 	view,
 };
