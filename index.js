801d03a7387d70c3ea778fa0ed30287c8977c027
Mukesh R <115150217+mukesher4@users.noreply.github.com>
2025-01-20 20:45:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/sessionStatus", require("./controllers/sessionController"));
-// app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 // app.use("/api/contacts", require("./routes/contactRoutes"));
 // app.use("/api/group/user", require("./routes/groupUserRoutes"));
 // app.use("/api/group", require("./routes/groupContactRoutes"));
