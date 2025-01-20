dea4260125349c43dc47e2d2a297316bb5f4b1ae
Mukesh R <115150217+mukesher4@users.noreply.github.com>
2025-01-20 19:06:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const connectDb = require("./config/dbConnection");
 connectDb();
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 
 const port = process.env.PORT || 5000;
 
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
 	console.log("Root route hit");
-	res.send("Express on Vercel");
+	res.json("Express on Vercel");
 });
 
 app.get("/api/sessionStatus", require("./controllers/sessionController"));
