f4930ee37a0d66f9b730a79c46fbb2af6aa6b3c2
Mukesh R <115150217+mukesher4@users.noreply.github.com>
2025-01-20 20:36:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const cors = require('cors');
+// const cors = require('cors');
 const express = require("express");
-const dotenv = require("dotenv").config();
-const errorHandler = require("./middleware/errorHandler");
-const connectDb = require("./config/dbConnection");
+// const dotenv = require("dotenv").config();
+// const errorHandler = require("./middleware/errorHandler");
+// const connectDb = require("./config/dbConnection");
 
 // connectDb();
 const app = express();
